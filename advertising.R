d49ae93a379813a55a80ebc23c8176c7c8ae910c
vbeakovic <valter.beakovic@gmail.com>
2017-01-11 20:50:00 +0100

    Basic R added.

--- advertising.R
+++ advertising.R
@@ -3,6 +3,54 @@ library(reshape2)
 library(dplyr)
 library(gridExtra)
 library(ggthemes)
+#### Data types ####
+# Integer
+a <- c(3L, 6L, 99L, -34L, 34L, 11111111L)
+class(a)
+
+# Numeric
+b <- c(3.14159, 2.71, -0.34567)
+class(b)
+
+# Logical
+c <- c(TRUE, FALSE)
+class(c)
+
+# Character
+d <- c(
+        "I love hamburgers",
+        "Matt is awesome",
+        "A Tweet is a string"
+)
+class(d)
+
+#### List ####
+al <- list(1L, 5.4, TRUE, "apple")
+class(al)
+
+
+#### Basic logical operators ####
+3 + 4 == 7 # TRUE
+3 - 2 == 7 # FALSE
+3 < 5 # TRUE
+5 < 3 # FALSE
+3 <= 3 # TRUE
+5 <= 3 # FALSE
+3 > 5 # FALSE
+5 > 3 # FALSE
+3 >= 3 # TRUE
+5 >= 7 # FALSE
+
+#### Example of basic R ####
+X <- 5.8
+Y <- 9.5
+
+X + Y == 15.3 # TRUE
+X - Y == 15.3 # FALSE
+if (X + Y == 15.3) {
+        print("True!")
+}
+
 advertising <- read.csv("./data/Advertising.csv")
 advertising_long <- melt(data = advertising[,2:5], id.vars = "Sales", 
                          measure.vars = c("TV", "Radio", "Newspaper"), 
