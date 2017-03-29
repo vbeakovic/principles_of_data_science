c99477706f34be986517c3d73b5bd1909f3d9f3c
vbeakovic <valter.beakovic@gmail.com>
2017-03-29 20:04:45 +0200

    linear model added.

--- salmon.R
+++ salmon.R
@@ -2,5 +2,15 @@ library(idr)
 library(ggplot2)
 
 #### Chapeter 1 - spawner-recruits model ####
+
+salmon_model <- lm(recruits ~ spawners, data = salmon)
+summary(salmon_model)
+
+salmon_model$coefficients
+
+# linear model
+# recruits = 1.44 * spawners + 436.08
+
+
 ggplot(salmon, aes(x = spawners, y = recruits)) + 
         geom_point(color = "darkblue", alpha = 0.6)
