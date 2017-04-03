235fbc741a9c56bd5837b72dd719e5cdc318087a
vbeakovic <valter.beakovic@gmail.com>
2017-04-03 22:05:36 +0200

    Yelp data set imported and basic exploration performed.

--- ch_3.R
+++ ch_3.R
@@ -1,3 +1,18 @@
+library(jsonlite)
+library(rjson)
 
-yelp_raw_data <- read_csv("https://raw.githubusercontent.com/vc1492a/Yelp-Challenge-Dataset/master/Prepped%20Data/output.csv")
+
+# read in offical Yelp review set (available at https://www.yelp.com/dataset_challenge)
+yelp_raw_data <- stream_in(file("yelp_academic_dataset_review.json"),pagesize = 10000)
+
+# take a look at the data
+head(yelp_raw_data)
+tail(yelp_raw_data)
+
+# take a look at the sructure
 str(yelp_raw_data)
+
+# find NA's
+sum(is.na(yelp_raw_data))
+
+summary(yelp_raw_data)
