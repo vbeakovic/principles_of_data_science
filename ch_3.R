39bac15e31057009493f38a30844ca1c485dba97
vbeakovic <valter.beakovic@gmail.com>
2017-04-04 17:15:21 +0200

    Yelp dataset business_id column summaries.

--- ch_3.R
+++ ch_3.R
@@ -1,6 +1,6 @@
 library(jsonlite)
 library(rjson)
-
+library(dplyr)
 
 # read in offical Yelp review set (available at https://www.yelp.com/dataset_challenge)
 yelp_raw_data <- stream_in(file("yelp_academic_dataset_review.json"),pagesize = 10000)
@@ -16,3 +16,19 @@ str(yelp_raw_data)
 sum(is.na(yelp_raw_data))
 
 summary(yelp_raw_data)
+
+# check class of yelp_raw_data
+class(yelp_raw_data)
+
+# a single data frame column (a vector)
+head(yelp_raw_data$business_id)
+class(yelp_raw_data$business_id)
+
+# exploring the business_id column
+yelp_raw_data %>% select(business_id) %>% 
+        summarise(count = n(), 
+                  uni = length(unique(business_id)), 
+                  top = names(table(yelp_raw_data$business_id)[which.max(table(yelp_raw_data$business_id))]),
+                  freq = max(table(yelp_raw_data$business_id))
+        )
+
