e29056210e0e1854f534609e2fb6398bae959c5c
vbeakovic <valter.beakovic@gmail.com>
2017-04-06 19:37:08 +0200

    Duplicate texts filtered in Yelp dataset.

--- ch_3.R
+++ ch_3.R
@@ -47,3 +47,19 @@ yelp_raw_data %>% select(text) %>%
                   top = names(table(yelp_raw_data$text)[which.max(table(yelp_raw_data$text))]),
                   freq = max(table(yelp_raw_data$text))
         )
+
+
+# filter reviewes with same text
+index <- names(which.max(table(yelp_raw_data$text)))
+
+duplicate_text <- yelp_raw_data %>% 
+                        filter(text == index)
+
+duplicate <- yelp_raw_data %>% group_by(text) %>% 
+        summarise(num = n()) %>% 
+        arrange(desc(num)) %>% 
+        filter(num > 1)
+duplicate_texts <- unique(duplicate$text)
+duplicate_texts_logical <- yelp_raw_data$text %in% duplicate_texts
+sum(duplicate_texts_logical)
+filtered_data_frame <- yelp_raw_data[duplicate_texts_logical, ]
